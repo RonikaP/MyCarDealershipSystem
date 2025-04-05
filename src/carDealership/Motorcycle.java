ee51f0f7fbeddde7ae1142175333231b574dfa87
Michael Persico <michael.a.persico@gmail.com>
2025-04-05 04:37:13 +0000

    Motorcycle and Sale javadoc

--- src/carDealership/Motorcycle.java
+++ src/carDealership/Motorcycle.java
@@ -2,19 +2,46 @@ package carDealership;
 
 import java.io.Serializable;
 
+/**
+ * Car Dealership System
+ *
+ * @author [Your Name] [Your ID]
+ * @author [Team Member Name] [Team Member ID]
+ * @since 1.8
+ */
 public class Motorcycle extends Vehicle implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private String handlebarType;
 
+	/**
+	 * Constructor for the Motorcycle class
+	 *
+	 * @param make - the manufacturer of the motorcycle
+	 * @param model - the model name of the motorcycle
+	 * @param color - the color of the motorcycle
+	 * @param year - the manufacturing year of the motorcycle
+	 * @param price - the price of the motorcycle in SAR (Saudi Arabian Riyal)
+	 * @param handlebarType - the type of handlebars on the motorcycle
+	 */
 	public Motorcycle(String make, String model, String color, int year, double price, String handlebarType) {
 		super(make, model, color, year, price);
 		this.handlebarType = handlebarType;
 	}
 
+	/**
+	 * Copy constructor that creates a new Motorcycle instance with the same attributes as another Motorcycle
+	 *
+	 * @param m - the Motorcycle object to copy
+	 */
 	public Motorcycle(Motorcycle m) {
 		this(m.make, m.model, m.color, m.year, m.price, m.handlebarType);
 	}
 
+	/**
+	 * Display detailed information about this motorcycle to the console
+	 * This method is used for providing vehicle information to users
+	 * based on role-based permissions as specified in project requirements
+	 */
 	public void displayInfo() {
 		String motorcycleName = make + " " + model + " " + year;
 		System.out.println("ID: " + id);
@@ -24,17 +51,31 @@ public class Motorcycle extends Vehicle implements Serializable {
 		System.out.println("Price: " + price + " SAR");
 	}
 
+	/**
+	 * Getter method for the motorcycle handlebar type
+	 *
+	 * @return the handlebar type of the motorcycle
+	 */
 	public String getHandlebarType() {
 		return handlebarType;
 	}
 
+	/**
+	 * Setter method for the motorcycle handlebar type
+	 *
+	 * @param handlebarType - the new handlebar type to set
+	 */
 	public void setHandlebarType(String handlebarType) {
 		this.handlebarType = handlebarType;
 	}
 
+	/**
+	 * Return a string representation of this motorcycle, including all attributes
+	 * This method is helpful for vehicle grouping and display functionality
+	 * 
+	 * @return a string containing all motorcycle information
+	 */
 	public String toString() {
-
 		return super.toString() + "\nHandelbarType: " + handlebarType;
 	}
-
 }
--- src/carDealership/Sale.java
+++ src/carDealership/Sale.java
@@ -3,6 +3,13 @@ package carDealership;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+/**
+ * Car Dealership System
+ *
+ * @author [Your Name] [Your ID]
+ * @author [Team Member Name] [Team Member ID]
+ * @since 1.8
+ */
 public class Sale implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private Vehicle vehicle;
@@ -10,6 +17,15 @@ public class Sale implements Serializable {
 	private String buyerContact;
 	private LocalDate saleDate;
 
+	/**
+	 * Constructor for the Sale class
+	 * Creates a record of a vehicle sale transaction
+	 *
+	 * @param vehicle - the vehicle that was sold
+	 * @param buyerName - the name of the buyer
+	 * @param buyerContact - the contact information of the buyer
+	 * @param saleDate - the date when the sale was completed
+	 */
 	public Sale(Vehicle vehicle, String buyerName, String buyerContact, LocalDate saleDate) {
 		this.vehicle = vehicle;
 		this.buyerName = buyerName;
@@ -17,36 +33,75 @@ public class Sale implements Serializable {
 		this.saleDate = saleDate;
 	}
 
+	/**
+	 * Getter method for the vehicle
+	 *
+	 * @return the vehicle associated with this sale
+	 */
 	public Vehicle getVehicle() {
 		return vehicle;
 	}
 
+	/**
+	 * Setter method for the vehicle
+	 *
+	 * @param vehicle - the new vehicle to associate with this sale
+	 */
 	public void setVehicle(Vehicle vehicle) {
 		this.vehicle = vehicle;
 	}
 
+	/**
+	 * Getter method for the buyer's name
+	 *
+	 * @return the name of the buyer
+	 */
 	public String getBuyerName() {
 		return buyerName;
 	}
 
+	/**
+	 * Setter method for the buyer's name
+	 *
+	 * @param buyerName - the new buyer name to set
+	 */
 	public void setBuyerName(String buyerName) {
 		this.buyerName = buyerName;
 	}
 
+	/**
+	 * Getter method for the buyer's contact information
+	 *
+	 * @return the contact information of the buyer
+	 */
 	public String getBuyerContact() {
 		return buyerContact;
 	}
 
+	/**
+	 * Setter method for the buyer's contact information
+	 *
+	 * @param buyerContact - the new buyer contact information to set
+	 */
 	public void setBuyerContact(String buyerContact) {
 		this.buyerContact = buyerContact;
 	}
 
+	/**
+	 * Getter method for the sale date
+	 *
+	 * @return the date when the sale was completed
+	 */
 	public LocalDate getSaleDate() {
 		return saleDate;
 	}
 
+	/**
+	 * Setter method for the sale date
+	 *
+	 * @param saleDate - the new sale date to set
+	 */
 	public void setSaleDate(LocalDate saleDate) {
 		this.saleDate = saleDate;
 	}
-
 }
